ab81f6334b0cc55046ea06a1c6f83b36b4c2a591
ksyp21 <ksypkarki@gmail.com>
2023-09-07 21:40:01 +0545

    CRUD operation

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
 const express = require('express')
+const { blogs } = require('./model/index')
 const app = express()
 
+
+
+// database connection 
+require("./model/index")
+
+// telling the nodejs to set view-engine to ejs
 app.set('view engine', 'ejs')
 
-//form bata data parse gar ani handel gar node lai
+
+// form bata data aairaxa parse gara or handle gar vaneko ho
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.render("blogs")
+// allBlog
+app.get("/", async (req, res) => {
+    //blogs vanney table bata vayejati sabai data dey vaneko 
+    const allBlogs = await blogs.findAll()
+
+
+    // blogs vanney key/name ma allBlogs/data pass gareko ejs file lai
+    res.render('blogs', { blogs: allBlogs })
 })
 
+//createBlog
 app.get("/createBlog", (req, res) => {
     res.render("createBlog")
 })
 
-app.post("/createBlog", (req, res) => {
-    console.log(req.body.title)
-    res.send("form submitted successfully")
+//createBlog Post
+app.post("/createBlog", async (req, res) => {
+
+    // second approach
+    // const {title,description,subtitle} = req.body
+    // first approach
+    const title = req.body.title
+    const description = req.body.description
+    const subTitle = req.body.subtitle
+
+    // database ma halnu paryo , database sanaga kehi operation await halnu parney hunchha 
+    // agadi , await halepaxi mathi async halnu parney hunchha 
+    await blogs.create({
+        title: title,
+        subTitle: subTitle,
+        description: description
+    })
+    res.redirect("/")
 })
+
+// database/table bata data kasari nikalney 
+
+
 app.listen(3000, () => {
-    console.log("Nodejs project started")
+    console.log("NodeJs project has started at port 3000")
 })
