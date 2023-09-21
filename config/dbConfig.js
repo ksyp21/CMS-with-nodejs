61ead3dc12e56b98af16f08a973b76d457e501b3
ksyp21 <ksypkarki@gmail.com>
2023-09-21 17:09:31 +0545

    database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,24 @@
+// FOR LOCAL ENVIRONMENT
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "sepcms",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   };
+
+
 module.exports = {
-    HOST: "localhost",
+    HOST: "containers-us-west-156.railway.app",
     USER: "root",
-    PASSWORD: "",
-    DB: "sepcms",
+    PASSWORD: "rR2gh1oVi2XvNMQEMEMS",
+    DB: "railway",
     dialect: "mysql",
     pool: {
         max: 5,
